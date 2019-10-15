5f2166d114bb5df81761fd902f72c3ce7b135370
Wes Fabella <wfabella@amazon.com>
2019-10-15 14:22:00 -0400

    gg group core definition version arn

--- cdk/cdk_stack.py
+++ cdk/cdk_stack.py
@@ -10,66 +10,54 @@ class CdkStack(core.Stack):
     def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
 
-        # The code that defines your stack goes here
+        # JITP new device certificate
+        # https://aws.amazon.com/blogs/iot/setting-up-just-in-time-provisioning-with-aws-iot-core/
+        # mosquitto_pub --cafile root.cert --cert deviceCertAndCACert.crt --key deviceCert.key -h <prefix>.iot.us-east-1.amazonaws.com -p 8883 -q 1 -t foo/bar -I xyz --tls-version tlsv1.2 -m "Hello" -d
+        certArn = 'arn:aws:iot:us-east-1:1234567890:cert/xyz'
+        region = core.Stack.of(self).region
+        accountId = core.Stack.of(self).account
+        print('region: %s' % region)
 
-        # create iot thing & policy
+
+        ## IOT THING BEGIN ##
+
+        # create iot thing
         gg_core = iot.CfnThing(self,
             'MyGGGCDKCore',
             thing_name='myggg-core'
         )
+        coreArn = 'arn:aws:iot:%s:%s:thing/%s' % (region, accountId, gg_core.thing_name)
         print('## Thing Name: %s' % gg_core.thing_name)
 
+        # create iot policy
         thing_policy = iot.CfnPolicy(self,
             'MyGGGCDKCorePolicy',
             policy_document=boto3_helper.policyDocument,
             policy_name='myggg-core-policy'
         )
-        print('## Policy Name: %s' % thing_policy.policy_name)
+        print('## Policy Name: %s' % thing_policy.policy_name)   
 
-        thing_policy = iot.CfnPolicy(self,
-            'MyGGGCDKGroupPolicy',
-            policy_document=boto3_helper.policyDocument,
-            policy_name='myggg-group-policy'
-        )      
-
-        # create greengrass group
-        ggg = gg.CfnGroup(self,
-            'MyGGGCDKGroup',
-            name='myggg-name'
-            #role_arn='myggg-role'  # optional
-        )
 
         # create iot thing certificate
         # uncomment boto3_helper.create_csr() if you want to use AWS keys and certs
         #certArn = boto3_helper.create_csr()  # ugh! - gens a new cert for each deployment
-       
+
         # using a self-signed cacert csr
-        cacert_csr=boto3_helper.read_csr()
-        iot_cacert = iot.CfnCertificate(self,
-            'MyGGGCDKCACertificate',
-            certificate_signing_request=cacert_csr,
-            status="ACTIVE"
-        )
-        print('## Show me Cert ARN: %s' % iot_cacert.attr_arn)
+        # cacert_csr=boto3_helper.read_csr()
+        # iot_cacert = iot.CfnCertificate(self,
+        #     'MyGGGCDKCACertificate',
+        #     certificate_signing_request=cacert_csr,
+        #     status="ACTIVE"
+        # )
+        # certArn=iot_cacert.attr_arn
+        # print('## Show me Cert ARN: %s' % iot_cacert.attr_arn)
 
         # attach policy to principal cert
         thing_policy_principal_props = iot.CfnPolicyPrincipalAttachmentProps(
             policy_name=thing_policy.policy_name,
-            principal=iot_cacert.attr_arn
+            principal=certArn
         )
         print('## Principal Cert Arn: %s' % thing_policy_principal_props.principal)
-
-        # core definition
-        gg_core_def = gg.CfnCoreDefinition(self,
-            'MyGGGCDKCoreDefinition',
-            name='myggg-core-definition'
-        )
-
-        gg_core_def_version = gg.CfnCoreDefinitionVersion(self,
-            'MyGGCDKCoreDefinitionVersion',
-            core_definition_id=gg_core_def.attr_id,
-            cores=[]
-        )
 
         # attach policy to iot certificate
         iot.CfnPolicyPrincipalAttachment(self,
@@ -88,3 +76,42 @@ class CdkStack(core.Stack):
         ).add_depends_on(
             resource=gg_core
         )
+
+        ## IOT THING END ##
+
+        ## GREENGRASS BEGIN ##
+
+        # Configure Greengrass Group & Associate IoT Thing to Greengrass Core
+
+        # core definition version
+        core_version_details = [{
+            'id': '1',
+            'certificateArn': certArn,
+            'thingArn': coreArn,
+            'shadowSync': True
+        }]
+
+        gg_core_def = gg.CfnCoreDefinition(self, 'MyGGGCDKCoreDefinition',
+            name='Raspberry_Pi_Core',
+            initial_version={ 'cores': core_version_details }
+        )
+        gg_core_def.add_depends_on(gg_core)
+
+        gg_core_def_version = gg.CfnCoreDefinitionVersion(self,
+            'MyGGCDKCoreDefinitionVersion',
+            core_definition_id=gg_core_def.attr_id,
+            cores=core_version_details
+        )
+
+        # create greengrass group
+        gg_group = gg.CfnGroup(self,
+            'MyGGGCDKGroup',
+            name='myggg-name',
+            initial_version={
+                'coreDefinitionVersionArn': gg_core_def.attr_latest_version_arn
+            }
+        )
+        gg_group.add_depends_on(gg_core_def)
+
+        # external ref: https://translate.google.com/translate?depth=1&hl=en&nv=1&rurl=translate.google.com&sl=auto&sp=nmt4&tl=en&u=https://dev.classmethod.jp/cloud/aws/aws-cdk-greengrass-rasberrypi/&xid=17259,15700002,15700023,15700186,15700191,15700256,15700259,15700262,15700265,15700271
+        ## GREENGRASS END ##
